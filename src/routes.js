056c78830e9bd8752095b71304ea6cf3f72a3529
kaybriyel <kaydirection1@gmail.com>
2021-01-22 13:58:58 +0700

    add ShopList

--- src/routes.js
+++ src/routes.js
@@ -26,6 +26,7 @@ import Unarchive from "@material-ui/icons/Unarchive";
 import Language from "@material-ui/icons/Language";
 // core components/views for Admin layout
 import DashboardPage from "views/Dashboard/Dashboard.js";
+import Shoplist from "views/Shoplist/Shoplist.js";
 import UserProfile from "views/UserProfile/UserProfile.js";
 import TableList from "views/TableList/TableList.js";
 import Typography from "views/Typography/Typography.js";
@@ -43,6 +44,13 @@ const dashboardRoutes = [
     rtlName: "لوحة القيادة",
     icon: Dashboard,
     component: DashboardPage,
+    layout: "/admin"
+  },
+  {
+    path: "/shoplist",
+    name: "Shoplist",
+    icon: Dashboard,
+    component: Shoplist,
     layout: "/admin"
   },
   {
