af4cb41c85f4e4835da8e172711403862a68e1c3
kaybriyel <kaydirection1@gmail.com>
2021-01-29 13:43:18 +0700

    add Myshop in navbar

---
+++ src/views/Myshop/Myshop.js
@@ -0,0 +1,30 @@
+import React, { useState, useEffect } from "react";
+// core components
+import Shop from "components/Shop/Shop.js";
+import {apiUrl} from 'variables/general.js';
+
+// will replace by shops from API
+
+export default function Myshop() {
+  const [shops, setShop] = useState([]);
+
+  useEffect(() => {
+    (async () => {
+      const res = await fetch(apiUrl + 'shops');
+      res.ok && setShop(await res.json());
+    })();
+  }, [shops.length]
+  );
+
+  return (
+    <div>
+      {
+        shops.map(shop => {
+          return (
+            <Shop key={shop.id} display={false} name={shop.name} items={shop.items} />
+          );
+        })
+      }
+    </div>
+  );
+}
